b0dffb04b0ec1d570bdd6376578a5df0f878005f
Miksio <mikolaj.loewnau@gmail.com>
2024-04-15 19:57:43 +0200

    First Commit

---
+++ app/src/main/java/com/example/kodqrv2/CaptureAct.java
@@ -0,0 +1,7 @@
+package com.example.kodqrv2;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity
+{
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kodqrv2"
+include(":app")
+ 
